ff05434500c73e323cf72f39fb819f3dc8682f3e
Aleksandar1601 <44443626+Aleksandar1601@users.noreply.github.com>
2018-11-13 10:20:46 +0100

    Add files via upload

---
+++ item5.php
@@ -0,0 +1,75 @@
+<?php
+include "php/header.php";
+?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Portfolio Item Heading -->
+        <h1 class="my-4">Izabrani proizvod
+        </h1>
+
+        <!-- Portfolio Item Row -->
+        <div class="row">
+
+            <div class="col-md-6">
+                <img class="img-fluid" src="img/shop/5.jpg" alt="KUMHO 205/75 R16C PowerGrip KC11 110/108Q">
+            </div>
+
+            <div class="col-md-6">
+                <h3 class="my-3">KUMHO 205/75 R16C PowerGrip KC11 110/108Q</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+                <h3 class="my-3">Specifikacije</h3>
+                <ul>
+                    <li>Povoljne</li>
+                    <li>Traktorska guma</li>
+                    <li>4x4</li>
+                    <li>Letnje šare</li>
+                </ul>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+        <!-- Related Projects Row -->
+        <h3 class="my-4">Pogledaj još:</h3>
+
+        <div class="row">
+
+            <div class="col-md-3 col-sm-6 mb-4">
+                <a href="item2.php">
+                    <img class="img-fluid" src="img/shop/2.jpg" alt="DUNLOP 160/60 ZR17 SPMAX RoadSmart 2 69W">
+                    DUNLOP 160/60 ZR17 SPMAX RoadSmart 2 69W
+                </a>
+            </div>
+
+            <div class="col-md-3 col-sm-6 mb-4">
+                <a href="item1.php">
+                    <img class="img-fluid" src="img/shop/1.jpg" alt="Hankook 185/75 R16C Winter RW06 104/102R">
+                    Hankook 185/75 R16C Winter RW06 104/102R
+                </a>
+            </div>
+
+            <div class="col-md-3 col-sm-6 mb-4">
+                <a href="item3.php">
+                    <img class="img-fluid" src="img/shop/3.jpg" alt="SAVA 155/70 R13 Eskimo S3+ 75T MS">
+                    SAVA 155/70 R13 Eskimo S3+ 75T MS
+                </a>
+            </div>
+
+            <div class="col-md-3 col-sm-6 mb-4">
+                <a href="item4.php">
+                    <img class="img-fluid" src="img/shop/4.jpg" alt="RIKEN 155/80 R13 SnowTime 79Q">
+                    RIKEN 155/80 R13 SnowTime 79Q
+                </a>
+            </div>
+
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+
+
+<?php include "php/footer.php"; ?>
